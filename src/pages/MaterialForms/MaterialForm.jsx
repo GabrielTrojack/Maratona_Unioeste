117d13ecbc835dbdc46d4ba459eb223847f28491
Gabriel Trojack <trojackgabriel@gmail.com>
2025-12-05 00:29:48 -0300

    materialform step 1

--- src/pages/MaterialForms/MaterialForm.jsx
+++ src/pages/MaterialForms/MaterialForm.jsx
@@ -3,23 +3,42 @@ import "./MaterialForm.css";
 import menu from "../../assets/menu.svg";
 
 import excluir from "../../assets/Excluir.svg";
-import editar from "../../assets/Editar.svg";
+import setasdir from "../../assets/setasdir.svg";
 
 
 const Material = () => {
   return (
     <div className="material-form">
       <form className="form" action="">
+        <div class="stepper">
+          <div class="step active"></div>
+          <div class="step"></div>
+          <div class="step"></div>
+        </div>
+
         <input className="modulo" placeholder="Titulo do modulo" type="text" />
+
         <div className="formName">
-          <p>aulas</p>
+          <p>Aulas</p>
           <button>Criar aula</button>
         </div>
-        <div className="linkAula">
-          <input className="modulo" placeholder="Titulo da aula" type="text" />
-          <input className="modulo" placeholder="Link da aula" type="text" />
-          <img src={excluir} onClick={()=>console.log("exclui aula")} />
+
+        <div className="aulas">
+          <div className="linkAula">
+            <input className="modulo" placeholder="Titulo da aula" type="text" />
+            <input className="modulo" placeholder="Link da aula" type="text" />
+            <img src={excluir} onClick={() => console.log("exclui aula")} />
+          </div>
+          
         </div>
+
+        <div className="downButton">
+          <button className="btn">
+            <p>Proximo</p>
+            <img src={setasdir} alt="" />
+          </button>
+        </div>
+
       </form>
     </div>
   );
